d9ce20d8082fdab9878dcc453a45be951c07c81a
Holubchuk <sashagolubchuk3@gmail.com>
2023-10-26 13:56:07 -0400

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,12 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
 const startBtn = document.querySelector('[data-start]');
 startBtn.disabled = true;
+
+let intervalId;
+let targetDate;
 
 flatpickr('#datetime-picker', {
   enableTime: true,
@@ -13,10 +17,54 @@ flatpickr('#datetime-picker', {
     const selectedDate = selectedDates[0];
 
     if (selectedDate < new Date()) {
-      window.alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
     } else {
-      console.log(selectedDates[0]);
       startBtn.disabled = false;
+      targetDate = selectedDate;
+      Notiflix.Notify.success('Well done');
     }
+  },
+});
+
+startBtn.addEventListener('click', () => {
+  if (targetDate) {
+    intervalId = setInterval(updateTimer, 1000);
+    startBtn.disabled = true;
   }
 });
+
+function updateTimer() {
+  const currentTime = new Date();
+  const diff = targetDate - currentTime;
+
+  if (diff <= 0) {
+    clearInterval(intervalId);
+  } else {
+    const { days, hours, minutes, seconds } = convertMs(diff);
+
+    document.querySelector('[data-days]').textContent = addLeadingZero(days);
+    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+    document.querySelector('[data-minutes]').textContent =
+      addLeadingZero(minutes);
+    document.querySelector('[data-seconds]').textContent =
+      addLeadingZero(seconds);
+  }
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
