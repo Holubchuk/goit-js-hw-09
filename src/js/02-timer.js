c976bca6035b8006f4315bf238770aeba01bd83b
Holubchuk <sashagolubchuk3@gmail.com>
2023-10-26 10:18:46 -0400

    task-2(first part)

---
+++ src/js/02-timer.js
@@ -0,0 +1,22 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate < new Date()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      console.log(selectedDates[0]);
+      startBtn.disabled = false;
+    }
+  }
+});
